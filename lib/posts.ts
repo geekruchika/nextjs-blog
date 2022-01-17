236722d15e0ec534b38d1dc72ce04079ba813891
ruchika <ruchika.sjv@gmail.com>
2022-01-17 16:03:03 +0530

    converted to TS

--- lib/posts.ts
+++ lib/posts.ts
@@ -23,39 +23,21 @@ export function getSortedPostsData() {
     // Combine the data with the id
     return {
       id,
-      ...matterResult.data
+      ...(matterResult.data as { date: string; title: string })
     }
   })
   // Sort posts by date
-  return allPostsData.sort(({ date: a }, { date: b }) => {
-    if (a < b) {
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
       return 1
-    } else if (a > b) {
-      return -1
     } else {
-      return 0
+      return -1
     }
   })
 }
 
-
-
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory)
-
-  // Returns an array that looks like this:
-  // [
-  //   {
-  //     params: {
-  //       id: 'ssg-ssr'
-  //     }
-  //   },
-  //   {
-  //     params: {
-  //       id: 'pre-rendering'
-  //     }
-  //   }
-  // ]
   return fileNames.map(fileName => {
     return {
       params: {
@@ -64,20 +46,8 @@ export function getAllPostIds() {
     }
   })
 }
-// export function getPostData(id) {
-//   const fullPath = path.join(postsDirectory, `${id}.md`)
-//   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
-//   // Use gray-matter to parse the post metadata section
-//   const matterResult = matter(fileContents)
-
-//   // Combine the data with the id
-//   return {
-//     id,
-//     ...matterResult.data
-//   }
-// }
-export async function getPostData(id) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
 
@@ -94,6 +64,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data
+    ...(matterResult.data as { date: string; title: string })
   }
 }
